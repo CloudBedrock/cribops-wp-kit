184cc2e56c0b7e4f1e72f38c90511608eed7f016
Jim Hankins <jhankins@cloudbedrock.com>
2025-12-28 20:10:35 -0500

    Fix ZIP permissions in GitHub Action (755/644)

--- cribops-wp-kit.php
+++ cribops-wp-kit.php
@@ -4,7 +4,7 @@
  * Plugin URI:  https://github.com/CloudBedrock/cribops-wp-kit
  * Short Description: WordPress site management and deployment toolkit for agencies.
  * Description: Comprehensive WordPress plugin management, license handling, and rapid site deployment using Prime Mover templates. Fork of LaunchKit Pro v2.13.2.
- * Version:     1.9.7
+ * Version:     1.9.8
  * Author:      CribOps Development Team
  * Author URI:  https://cribops.com
  * Text Domain: cwpk
@@ -41,7 +41,7 @@ if (!class_exists('CribOpsWPKit')) {
 
     class CribOpsWPKit {
 
-        const VERSION = '1.9.7';
+        const VERSION = '1.9.8';
 
         public function __construct() {
             register_activation_hook(__FILE__, array($this, 'check_and_delete_original_plugin'));
